74cd88477f9d4fc45397b6dc237530a1e6dbab70
Michael Dpenha <michaeld@xpanxion.co.in>
2018-06-11 13:54:48 +0530

    Commit for Pagination component

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -6,10 +6,114 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent implements OnInit {
+  @Input() page: number; // the current page
+  @Input() count: number; // how many total items there are in all pages
+  @Input() perPage: number; // how many items we want to show per page
+  @Input() pagesToShow: number; // how many pages between next/prev
+  @Input() pageLimitArray: any = []; //Number of Records per page
+ 
+  @Output() goPrev = new EventEmitter<boolean>(); // Event emitted when click on previous
+  @Output() goNext = new EventEmitter<boolean>(); // Event emitted when click on next 
+  @Output() goPage = new EventEmitter<number>(); // Event emitted when click on the page number
+  @Output() changeInPerPage = new EventEmitter<number>(); // Event emitted when change in no of records per page
 
+  public currentPageCount : number;
   constructor() { }
 
   ngOnInit() {
+    this.calculateCurrentPageCount();
   }
-
+  /**
+   * This method will trigger when any of the input arguments changes
+   * @param changes
+   */
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['count']) {
+      this.count = this.count;
+      this.calculateCurrentPageCount();
+    }
+  }
+  /**
+   * calculateCurrentPageCount
+   */
+  public calculateCurrentPageCount = () => {
+    this.currentPageCount = (!this.lastPage()) ? this.perPage * this.page : this.count;
+  }
+  /**
+   * getMin
+   */
+  public getMin = (): number => {
+    return ((this.perPage * this.page) - this.perPage) + 1;
+  }
+  /**
+   * getMax
+   */
+  public getMax = (): number => {
+    let max = this.perPage * this.page;
+    if (max > this.count) {
+      max = this.count;
+    }
+    return max;
+  }
+  /**
+   * onPage
+   */
+  public onPage = (n: number): void => {
+    this.page = n;
+    this.calculateCurrentPageCount();
+    this.goPage.emit(n);
+  }
+  /**
+   * onPrev
+   */
+  public onPrev = (): void => {
+    this.page = this.page - 1;
+    this.calculateCurrentPageCount();
+    this.goPrev.emit(true);
+  }
+  /**
+   * onNext 
+   */
+  public onNext = (next: boolean): void => {
+    this.page = this.page + 1;
+    this.calculateCurrentPageCount();
+    this.goNext.emit(next);
+  }
+  /**
+   * totalPages
+   */
+  public totalPages = (): number => {
+    return Math.ceil(this.count / this.perPage) || 0;
+  }
+  /**
+   * lastPage
+   */
+  public lastPage = (): boolean => {
+    return this.perPage * this.page >= this.count;
+  }
+  /**
+   * onPageSelect
+   */
+  public onPageSelect = (...n) => {
+    this.perPage = Number(n[0]);
+    this.calculateCurrentPageCount();
+    this.changeInPerPage.emit(this.perPage);
+  }
+  /**
+   * getPages
+   */
+  public getPages = (): number[] => {
+    const totalPages = this.totalPages();
+    //const c = Math.ceil(this.count / this.perPage);
+    const page = this.page || 1;
+    const pagesToShow = this.pagesToShow || 9;
+    const pages: number[] = [];
+    pages.push(page);
+    const iteration = pagesToShow - 1;
+    for (let i = 0; i < iteration; i++) {
+      (pages && pages.length < pagesToShow && Math.min.apply(null, pages) > 1) ? pages.push(Math.min.apply(null, pages) - 1) : (pages.length < pagesToShow && Math.max.apply(null, pages) < totalPages) ? pages.push(Math.max.apply(null, pages) + 1) : '' ;
+    }
+    pages.sort((a, b) => a - b);
+    return pages;
+  }
 }

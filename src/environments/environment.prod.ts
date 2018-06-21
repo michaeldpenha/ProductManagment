11b1222d5bcc01033fc325674eac8991bb473557
Niraj Chavan <nirajc@xpanxion.co.in>
2018-06-21 18:39:05 +0530

    http error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   env: 'prod',
-  orderProcessorBaseUrl : 'http://dev-op-api.centralus.cloudapp.azure.com/order-processor/',
+  orderProcessorBaseUrl : 'http://dev-op-api.centralus.cloudapp.azure.com/order-processor/webapi/',
   batchProcessorBaseUrl : 'http://dev-batch-api.centralus.cloudapp.azure.com/batch-processor'
 };
--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   env: 'qa',
-  orderProcessorBaseUrl: '',
-  batchProcessorBaseUrl: ''
+  orderProcessorBaseUrl: 'http://dev-op-api.centralus.cloudapp.azure.com/order-processor/webapi/',
+  batchProcessorBaseUrl: 'http://dev-batch-api.centralus.cloudapp.azure.com/batch-processor'
 };
